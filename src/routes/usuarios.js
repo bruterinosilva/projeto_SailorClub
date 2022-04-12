07d42c65f1348a64e47b9d05312f81faf088a507
marcorover <marcomorover@gmail.com>
2022-04-12 11:30:24 -0300

    adicionando comentários e corrigindo o css de cadastro e index

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -11,6 +11,7 @@ router.get("/listar", function (req, res) {
     usuarioController.listar(req, res);
 });
 
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
 router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
 })
