e518723966661c4c4462ae3613de8b02f18ec172
Bruna Silva <bruna.silva@sptech.school>
2022-05-23 00:36:32 -0300

    subindo atualizações do projeto

---
+++ main.js
@@ -0,0 +1,75 @@
+const cardBoard = document.querySelector("#cardboard");
+const imgs = [
+    "img-1.png",
+    "img-2.png",
+    "img-3.png",
+    "img-4.png",
+    "img-5.png",
+    "img-6.png"
+];
+
+let cardHTML = "";
+
+imgs.forEach(img => {
+    cardHTML += `<div class="memory-card" data-card="${img}">
+    <img class="front-face" src="img/${img}"/>
+    <img class="back-face" src="img/sailor_login_im.png">
+  </div>`;
+});
+
+cardBoard.innerHTML = cardHTML + cardHTML;
+
+const cards = document.querySelectorAll(".memory-card");
+let firstCard, secondCard;
+let lockCards = false;
+
+function flipCard() {
+    if (lockCards) return false;
+    this.classList.add("flip");
+    this.childNodes[1].classList.add("show-img")
+    if (!firstCard) {
+        firstCard = this;
+        return false;
+    }
+
+    secondCard = this;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+
+    !isMatch ? unFlipCards() : resetCards(isMatch);
+}
+
+function unFlipCards() {
+    lockCards = true;
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        firstCard.childNodes[1].classList.remove("show-img")
+
+        secondCard.classList.remove("flip");
+        secondCard.childNodes[1].classList.remove("show-img")
+       
+        resetCards();
+    }, 1000);
+}
+
+function resetCards(isMatch = false) {
+    if (isMatch) {
+        firstCard.removeEventListener("click", flipCard);
+        secondCard.removeEventListener("click", flipCard);
+    }
+
+    [firstCard, secondCard, lockCards] = [null, null, false];
+}
+
+cards.forEach(card => card.addEventListener("click", flipCard));
+
+(function aleatory() {
+    cards.forEach(card => {
+        let rand = Math.floor(Math.random() * 12);
+        card.style.order = rand
+    });
+})();
---
+++ quiz.js
@@ -0,0 +1,82 @@
+var sailor1 = 0
+var sailor2 = 0
+var sailor3 = 0
+var sailor4 = 0
+var sailor5 = 0
+var contador = 1
+
+function startQuiz() {
+    var botaoIniciar = document.getElementById("startQuiz")    
+    var pergunta1 = document.getElementById("pergunta1")
+    
+    pergunta1.classList.add("active")    
+    botaoIniciar.remove()
+}
+
+
+function addSailor1() {
+    sailor1++
+    responder()
+}
+function addSailor2() {
+    sailor2++
+    responder()
+}
+function addSailor3() {
+    sailor3++
+    responder()
+}
+function addSailor4() {
+    sailor4++
+    responder()
+}
+function addSailor5() {
+    sailor5++
+    responder()
+}
+
+
+function responder() {
+    var quiz = document.getElementById("quiz")
+    var perguntas = document.querySelectorAll(".pergunta")
+    var qtdPerguntas = quiz.children.length
+    var n = 0
+
+    if (contador < qtdPerguntas) {
+
+        
+        while (n < perguntas.length) {
+            perguntas[n].classList.remove("active")
+            n++
+        }
+        
+        quiz.children[contador].classList.add("active")
+        contador++
+    }else{
+        quiz.innerHTML = 
+        `
+        <h2>Teste finalizado</h2>
+                <p>Clique no botão abaixo para exibir os resultados</p>
+                <button onclick="results()">Mostrar resultados</button>
+        `
+    }
+
+
+
+
+}
+
+function results() {
+    quiz.innerHTML = 
+        `
+            <h2>Resultado</h2>
+            <p>Sailor Moon: ${sailor1} </p>
+            <p>Sailor Vênus: ${sailor2} </p>
+            <p>Sailor Netuno: ${sailor3} </p>
+            <p>Sailor Call: ${sailor4} </p>
+            <p>Sailor Mall: ${sailor5} </p>
+        
+        
+        `
+}
+
